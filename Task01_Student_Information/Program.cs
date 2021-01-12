5aaaec0649ff08917ff3e15f8998a3fe7848d2b1
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-12 22:08:00 +0200

    Add files via upload

---
+++ Task01_Student_Information/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _Task01_Student_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name = Console.ReadLine();
+            string age = Console.ReadLine();
+            string grade = Console.ReadLine();
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade}");
+           
+        }
+    }
+}
