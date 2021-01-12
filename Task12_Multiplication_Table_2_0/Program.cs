e2bf9e58d52d60a2d885be6250e90fb1a67ecb5d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-12 22:16:33 +0200

    Add files via upload

---
+++ Task12_Multiplication_Table_2_0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task12_Multiplication_Table_2_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            
+            if (b > 10 )
+                {
+                    Console.WriteLine($"{a} X {b} = {a * b}");
+                }
+            else
+                {
+                    for (int i = b; i <= 10; i++)
+                    {
+                        Console.WriteLine($"{a} X {i} = {a * i}");
+                    }
+                }
+        }
+    }
+}
