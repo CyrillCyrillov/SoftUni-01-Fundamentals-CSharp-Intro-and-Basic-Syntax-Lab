bb0653c65c0e64003f797f162e1113a7705bd63b
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-12 22:11:56 +0200

    Add files via upload

---
+++ Task06_Foreign_Languages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task06_Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string state = Console.ReadLine();
+
+            switch (state)
+            {
+                case "England":
+                case "USA":
+                     Console.WriteLine("English");
+                     break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
