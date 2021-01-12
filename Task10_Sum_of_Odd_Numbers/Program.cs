470909be643b057d343c6ad9f937eb9b0f8c6a76
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-12 22:14:44 +0200

    Add files via upload

---
+++ Task10_Sum_of_Odd_Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task10_Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int number = 1;
+            int count = 0;
+            while (count < n)
+            {
+                if (number % 2 != 0)
+                    {
+                        Console.WriteLine(number);
+                        sum += number;
+                        count += 1;
+                    }
+                
+                number += 1;
+            }
+            
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
