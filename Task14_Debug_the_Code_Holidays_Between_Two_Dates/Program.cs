4d5a5634a17fd04e52049e71c70d5d6c7ee19957
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-12 22:19:06 +0200

    Add files via upload

---
+++ Task14_Debug_the_Code_Holidays_Between_Two_Dates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace Delete
+{
+
+    class Program
+    {
+        public static System.Globalization.CultureInfo InvariantCulture;
+        static void Main(string[] args)
+        {
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine(),
+                "d.MM.yyyy", CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(Console.ReadLine(),
+                "d.MM.yyyy", CultureInfo.InvariantCulture);
+            int holidaysCount = 0;
+
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    holidaysCount += 1;
+                }
+            }
+
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
+
